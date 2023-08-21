359e9acb9f5a86a57b8784cf5d6ebf8f636c9dd2
khairul-01 <khairul431743@gmail.com>
2023-08-21 22:04:01 +0600

    coupon and total  part calculated with js code

--- js/purchaseCalculation.js
+++ js/purchaseCalculation.js
@@ -1,16 +1,50 @@
+// const makePurchase = document.getElementById('btn-purchase');
+// const totPrice = getValue('total-price');
+// if (totPrice > 0) {
+//    makePurchase.removeAttribute('disabled');
+// }
+
 function getValue(inputId) {
    const element = document.getElementById(inputId);
    const elementStingValue = element.innerText;
    const elementValue = parseFloat(elementStingValue);
    return elementValue;
 }
+function setValue(inputId, value){
+   const element = document.getElementById(inputId);
+   element.innerText = value;
+}
 function totalPrice(inputValue) {
    const element = document.getElementById('total-price');
    const elementStingValue = element.innerText;
    const elementValue = parseFloat(elementStingValue);
+   const makePurchase = document.getElementById('btn-purchase');
    const newValue = elementValue + inputValue;
+   if (newValue > 0) {
+      makePurchase.removeAttribute('disabled');
+   }
    element.innerText = newValue;
+   const couponBtn = document.getElementById('btn-coupon');
+   if(newValue >= 200){
+      couponBtn.removeAttribute('disabled');
+   }
+   setValue('total-value', newValue);
 }
+document.getElementById('btn-coupon').addEventListener('click', function(){
+   const totalPrice = getValue('total-price');
+   console.log(totalPrice);
+   const couponCode = document.getElementById('coupon-code');
+   if (totalPrice >= 200 && couponCode.value === 'SELL200'){
+      const discount = totalPrice*.2;
+      setValue('discount-value', discount);
+      const total = totalPrice - discount;
+      setValue('total-value', total);
+   }
+   else{
+      alert('To get discount, please insert input correctly');
+      setValue('total-value', totalPrice);
+   }
+})
 function addToLog(productName){
    const element = document.getElementById('product-history');
    const p = document.createElement('p');
@@ -51,3 +85,5 @@ setHistoryPrice('sofa', 'sofa-price', 'Flexible Sofa');
 document.getElementById('go-home').addEventListener('click', function() {
    window.location.href = 'index.html';
 })
+
+
